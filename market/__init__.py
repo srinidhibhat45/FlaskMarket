d4709a012cf0ed8355898eb0bebc4f043e708207
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-05-28 21:02:46 +0530

    verification and password hash implemented

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = '6424cbdee5ddacdf6c82fd1b'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
