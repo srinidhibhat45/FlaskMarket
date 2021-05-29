40e0288b580c579872a3a5edd5771848a2f14321
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-05-29 14:07:20 +0530

    changes to login

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,5 +11,6 @@ app.config['SECRET_KEY'] = '6424cbdee5ddacdf6c82fd1b'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
