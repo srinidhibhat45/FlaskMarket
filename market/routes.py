3f7326543f7bbf6387918f6c898b222b850b3e5b
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-05-29 14:07:30 +0530

    changes to login

--- market/routes.py
+++ market/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user
 
 
 @app.route('/')
@@ -36,7 +37,14 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
-
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Successfully logged in as {attempted_user.username}', category='success')
+            return redirect('/market')
+        else:
+            flash('Username and password do not match! Please try again', category='danger')
     return render_template('login.html', form=form)
 
 
