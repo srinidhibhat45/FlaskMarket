2dc6ed6bac725442fd09ad6ee230aeb0b79086e3
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-05-31 23:02:33 +0530

    fixed salt error

--- market/models.py
+++ market/models.py
@@ -21,10 +21,11 @@ class User(db.Model, UserMixin):
 
     @password.setter
     def password(self, plain_text_password):
-        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-p8')
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
     def check_password_correction(self, attempted_password):
-        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+        if bcrypt.check_password_hash(self.password_hash, attempted_password):
+            return True
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
