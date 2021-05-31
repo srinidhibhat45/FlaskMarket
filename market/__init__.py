d28c64d083abf41f5230ad306d5dc03ed96f2080
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-06-01 00:08:25 +0530

    made login flashes visible and added login required for market

--- market/__init__.py
+++ market/__init__.py
@@ -12,5 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-
+login_manager.login_view = "login_page"
+login_manager.login_message_category = 'warning'
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -14,6 +14,7 @@ def home_page():
 
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -28,6 +29,8 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category="success")
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
@@ -46,6 +49,13 @@ def login_page():
         else:
             flash('Username and password do not match! Please try again', category='danger')
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have Been logged out!', category='warning')
+    return redirect('/')
 
 
 
